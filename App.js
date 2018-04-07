4623921a1093bd337b512776ee198bd4de007a77
Liam Morley <liam@carpeliam.com>
2018-04-07 19:39:47 -0400

    Play youtube videos
    - relies on API key from a YOUTUBE_API_KEY environment variable
    - right now, the mechanism for returning to the main screen is by closing the video
    - loads videos from a local JSON file - in the future, we can add additional metadata there

--- App.js
+++ App.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import YouTube from 'react-native-youtube';
+import videos from './videos.json';
 
-export default class App extends React.Component {
+const youtubeApiKey = process.env.YOUTUBE_API_KEY;
+
+class Player extends React.Component {
   render() {
+    const navigation = this.props.navigation;
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <YouTube
+          apiKey={youtubeApiKey}
+          videoId={navigation.state.params.videoId}
+          play={true}
+          fullscreen={true}
+          showFullscreenButton={false}
+          onChangeFullscreen={e => e.isFullscreen || navigation.goBack()}
+          style={{ width: 0, height: 0 }}
+        />
       </View>
     );
   }
 }
+
+class App extends React.Component {
+  render() {
+    const navigation = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        {videos.map(video => (
+          <Button
+            key={video.youtubeVideoId}
+            onPress={() => navigation.navigate('Player', { videoId: video.youtubeVideoId })}
+            title={`View ${video.youtubeVideoId}`}
+          />
+        ))}
+      </View>
+    );
+  }
+}
+
+export default StackNavigator({
+  Home: { screen: App },
+  Player: { screen: Player },
+});
 
 const styles = StyleSheet.create({
   container: {
