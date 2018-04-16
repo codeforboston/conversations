6ad672e30284bb414f5cb84e99587601257fb0d2
Brian Sanders <bdougsand+cfb@gmail.com>
2018-04-15 22:06:09 -0400

    Added a button bar at the bottom of the home page

--- App.js
+++ App.js
@@ -4,6 +4,11 @@ import { StackNavigator } from 'react-navigation';
 import YouTube from 'react-native-youtube';
 import { videos } from './config';
 
+import AboutPage from "./page/About.js";
+
+import TextButton from "./component/TextButton.js";
+
+
 const youtubeApiKey = process.env.YOUTUBE_API_KEY;
 
 class Player extends React.Component {
@@ -28,18 +33,24 @@ class Player extends React.Component {
 class ObjectChooser extends React.Component {
   render() {
     const navigation = this.props.navigation;
-    return (
-      <View style={styles.objectChooser}>
-        {videos.map(video => (
-          <TouchableHighlight
-            key={video.youtubeVideoId}
-            onPress={() => navigation.navigate('Player', { videoId: video.youtubeVideoId })}
-            style={{ padding: 15 }}
-          >
-            <Image source={video.asset} resizeMode="cover" style={{ width: 100, height: 100 }} />
-          </TouchableHighlight>
-        ))}
-      </View>
+      return (
+          <View style={styles.pageContainer}>
+              <View style={styles.objectChooser}>
+                  {videos.map(video => (
+                      <TouchableHighlight
+                          key={video.youtubeVideoId}
+                          onPress={() => navigation.navigate('Player', { videoId: video.youtubeVideoId })}
+                          style={styles.iconButtonStyle}
+                          underlayColor="rgba(255, 255, 255, 0.5)"
+                          >
+                          <Image source={video.asset} resizeMode="contain" style={styles.iconImageStyle} />
+                      </TouchableHighlight>
+                  ))}
+              </View>
+              <View style={styles.buttonContainer}>
+                  <TextButton navigation={navigation} route="About">About</TextButton>
+              </View>
+          </View>
     );
   }
 }
@@ -47,12 +58,38 @@ class ObjectChooser extends React.Component {
 export default StackNavigator({
   Home: { screen: ObjectChooser },
   Player: { screen: Player },
+  About: AboutPage.navConfig
+}, {
+
+    headerMode: "none"
 });
 
 const styles = StyleSheet.create({
+    iconButtonStyle: {
+        padding: 15
+    },
+
+    iconImageStyle: {
+        width: 100,
+        height: 100
+    },
+
+    pageContainer: {
+        flex: 1,
+        flexDirection: "column"
+    },
+
+    buttonContainer: {
+        height: 40,
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+
   objectChooser: {
     flex: 1,
     flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'space-between',
     backgroundColor: '#82c9de',
     alignItems: 'flex-start',
   },
