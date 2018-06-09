f68209d3f3cabbf665c9f415ec18e70b2d7a5e2e
Brian Sanders <bdougsand+cfb@gmail.com>
2018-06-09 08:54:33 -0400

    Begin button animation on press in instead of after release

--- component/Button.js
+++ component/Button.js
@@ -67,24 +67,67 @@ export class Button extends Component {
         );
     }
 
-    makePressHandler() {
+    _queue = []
+
+    _queueAnimation = (...animations) => {
+        this._queue.push(...animations);
+        if (!this._animation) {
+            this._nextAnimation();
+        }
+    }
+
+    _clearQueue = (_) => {
+        if (this._animation)
+            this._animation.stopAnimation();
+        this._queue = [];
+    }
+
+    _nextAnimation = () => {
+        if (this._queue && this._queue.length) {
+            var next = this._queue.shift();
+
+            if (typeof next === "function") {
+                next();
+                this._nextAnimation();
+            } else {
+                this._animation = next;
+                next.start(this._nextAnimation);
+            }
+        } else {
+            this._animation = null;
+        }
+    }
+
+    onPressIn = (e) => {
+        let {pressAnimation} = this.props;
+
+        if (pressAnimation === "spring") {
+            this._queueAnimation(
+                Animated.timing(this.state.scale, {toValue: 0.7,
+                                                   duration: 75,
+                                                   easing: Easing.linear()}));
+        }
+    }
+
+    onPressOut = (e) => {
+        let {scale} = this.state;
+        let {pressAnimation} = this.props;
+
+        if (pressAnimation === "spring") {
+            this._queueAnimation(
+                Animated.sequence([
+                    Animated.timing(scale, {toValue: 1.2, duration: 200, easing: Easing.linear()}),
+                    Animated.timing(scale, {toValue: 1, duration: 100, easing: Easing.linear()})
+                ]));
+        }
+    }
+
+    onPress = (...args) => {
         let {pressAnimation} = this.props,
             handler = makePressHandler(this.props);
 
-        if (pressAnimation === "spring") {
-            // Run the animation before running the handler
-            return ((...args) => {
-                let {scale} = this.state;
-
-                Animated.sequence([
-                    Animated.timing(scale, {toValue: 0.7, duration: 100, easing: Easing.linear()}),
-                    Animated.timing(scale, {toValue: 1.2, duration: 200, easing: Easing.linear()}),
-                    Animated.timing(scale, {toValue: 1, duration: 100, easing: Easing.linear()})
-                ]).start(() => handler(...args));
-            })
-        } else {
-            // Run right away:
-            return handler;
+        if (pressAnimation) {
+            this._queueAnimation(() => handler(...args));
         }
     }
 
@@ -96,9 +139,9 @@ export class Button extends Component {
             <TouchableOpacity
                 activeOpacity={activeOpacity}
                 disabled={disabled}
-                onPressIn={() => this.setState({ active: true })}
-                onPressOut={() => this.setState({ active: false })}
-                onPress={this.makePressHandler()}
+                onPressIn={this.onPressIn}
+                onPressOut={this.onPressOut}
+                onPress={this.onPress}
                 style={style}
                 {...props}
             >
