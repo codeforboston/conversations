a873f21c3b763d48d2922cb52bf86ddc06934355
Steve Man <stevemanbu@gmail.com>
2018-06-10 13:30:14 -0400

    Initial Commit of Nav Bar
    Very preliminary version of nav bar, does not have functionality for
    contextual help or remnant icon logic (ie. remnant icon appearing after
    viewing 2 videos).

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { StyleSheet, View, TouchableHighlight, Image, Dimensions } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { StyleSheet, View, TouchableHighlight, Image, Dimensions, Text  } from 'react-native';
+import { createBottomTabNavigator, TabNavigator, TabBarBottom } from 'react-navigation';
 import YouTube from 'react-native-youtube';
 import { videos } from './config';
 import ObjectChooser from './ObjectChooser';
-import HomeScreen from './HomeScreen';
-
 import AboutPage from "./page/About.js";
-
+import HomeScreen from "./HomeScreen.js";
+import { Button } from "./component/Button.js";
 
 // const youtubeApiKey = process.env.YOUTUBE_API_KEY;
 const youtubeApiKey = "AIzaSyBVwmuzixD7KGYsuP_2840WcXNFk1SnrUU";
@@ -21,7 +20,7 @@ class Player extends React.Component {
       <View>
         <YouTube
           apiKey={youtubeApiKey}
-          videoId={navigation.state.params.videoId}
+          videoId={this.props.navigation.getParam('videoId','')}
           play={true}
           fullscreen={true}
           showFullscreenButton={false}
@@ -33,18 +32,84 @@ class Player extends React.Component {
   }
 }
 
-export default StackNavigator({
-  Home: {
-    screen: HomeScreen,
+class tempCamera extends React.Component{
+  render(){
+    return(
+      <Text> {this.props.navigation.getParam('videoId',"")} </Text>
+    )
+  }
+}
+
+class tempSettings extends React.Component{
+  render(){
+    return(
+      <Text> Settings </Text>
+    )
+  }
+}
+
+export default createBottomTabNavigator({
+  HomeScreen: {screen: HomeScreen,
     navigationOptions: {
-      header: null,
+      tabBarVisible: false,
+      tabBarIcon: null,
     },
   },
-    Chooser: {
-        screen: ObjectChooser,
-    },
-  Player: { screen: Player },
-  About: AboutPage.navConfig
+  Chooser: {screen: ObjectChooser},
+  tempCamera: { screen: tempCamera },
+  Player: { screen: Player,
+    navigationOptions: {
+      tabBarVisible: false,
+      tabBarIcon: null,
+    }, },
+  tempSettings: { screen: tempSettings },
+  About: { screen: AboutPage },
 }, {
-    headerMode: "none"
-});
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, tintColor }) => {
+      const { routeName, params  } = navigation.state;
+
+      let finishedIcon;
+
+      if (routeName == 'Chooser' ) {
+        finishedIcon = require('./assets/TapHoldOff.png');
+        if (focused) {
+            finishedIcon = require('./assets/TapHoldOn.png')
+        }
+      }
+      else if (routeName == 'tempCamera' ) {
+        finishedIcon = require('./assets/SubmitVideoOff.png');
+        if (focused) {
+            finishedIcon = require('./assets/SubmitVideoOn.png')
+        }
+      }
+      else if (routeName == 'tempSettings' ) {
+        finishedIcon = require('./assets/SettingsOff.png');
+        if (focused) {
+            finishedIcon = require('./assets/SettingsOn.png')
+        }
+      }
+      else if (routeName == 'About' ) {
+        finishedIcon = require('./assets/AudioHelpOff.png');
+        if (focused) {
+            finishedIcon = require('./assets/AudioHelpOn.png')
+        }
+      } 
+
+      if (navigation.state.params) {
+        console.log(params);
+        finishedIcon = require('./assets/TapHoldOff.png');
+      }
+      
+      return <Button image={finishedIcon} />;
+    },
+  }),
+  tabBarOptions: {
+    showLabel : false,
+    style: {
+      backgroundColor: 'blue', 
+    }
+  }, 
+  animationEnabled: true,
+}
+);
