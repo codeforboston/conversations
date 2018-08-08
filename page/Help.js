59e8dcb0e72c110226d58d81bcf24d06df980d18
kavyakrishna211 <41350323+kavyakrishna211@users.noreply.github.com>
2018-08-07 20:49:51 -0400

    Update Help.js
    Replaced English words with Hindi

--- page/Help.js
+++ page/Help.js
@@ -257,7 +257,7 @@ const translations = {
             audio: '',
             text: (nav) => (
                 <TouchableOpacity onPress={() => nav.navigate('Settings', {targetSection:'about'})}>
-                    <Text>सेटिंग्स से लिंक करें</Text>
+                    <Text>सेटिंग पेज पर टेक्स्ट के बारे में देखें</Text>
                 </TouchableOpacity>
             )
         },
@@ -266,7 +266,7 @@ const translations = {
             audio: '',
             text: (nav) => (
                 <TouchableOpacity onPress={() => nav.navigate('Settings', {targetSection:'credits'})}>
-                    <Text>सेटिंग्स से लिंक करेंस</Text>
+                    <Text>सेटिंग्स पेज पर क्रेडिट देखें</Text>
                 </TouchableOpacity>
             )
         },
